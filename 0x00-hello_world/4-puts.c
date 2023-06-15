29f9e1f61269ed7fbf42065540f84e66d2db6a63
othmanoach <othmanoach@gmail.com>
2023-06-14 22:45:20 -0700

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *main - opening a main for my first program in c
- *
- *creating a simple script to show a sentence
- *
+ *main - opening a main for my first program in c *
  *Return : return value 0 to exit
  */
 int main(void)
 {
-	char displayed[52]; /* displayed is an array with 52 char */
-/* strcpy to copy sentence into the array displayed */
+	char displayed[52];
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	/* puts to display content of array displayed */
 	puts(displayed);
-	/* returning 0 will exit*/
 	return (0);
 }
