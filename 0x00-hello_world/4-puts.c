c1d1ed806eb2d70bda33318664c8cabc188e531d
othmanoach <othmanoach@gmail.com>
2023-06-14 22:47:54 -0700

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
  *main - opening a main for my first program in c *
+ *
+ * 
  *Return : return value 0 to exit
  */
 int main(void)
 {
+
 	char displayed[52];
+
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
 	puts(displayed);
 	return (0);
