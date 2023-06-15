c5f8e1a94a8b51e4e91cb376d1e3aeff1cbe9f08
othmanoach <othmanoach@gmail.com>
2023-06-14 23:26:26 -0700

    using printf for first time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - the Main function
+*
+*
+*Return: return 0 alltime
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
