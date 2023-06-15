4c6e76436a88f3c991271b6974689da7a58ea63c
othmanoach <othmanoach@gmail.com>
2023-06-14 22:34:35 -0700

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * opening a main for my first program in c
+ * creating a simple script to show a sentence
+ */
 int main(void)
 {
-       	/** main taking an integer value without having any argument(void)*/
 	char displayed[52]; /* displayed is an array that can hold up to 52 character */
 /* strcpy to copy sentence into the array displayed */
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
