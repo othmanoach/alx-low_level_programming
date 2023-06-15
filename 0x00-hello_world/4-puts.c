963f03a2ebf95c3a7830e3751763170bc5941caa
othmanoach <othmanoach@gmail.com>
2023-06-14 22:32:07 -0700

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@ int main(void)
 {
        	/** main taking an integer value without having any argument(void)*/
 	char displayed[52]; /* displayed is an array that can hold up to 52 character */
-/* we are using strcpy to copy the sentence into the array displayed */
+/* strcpy to copy sentence into the array displayed */
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	/* puts is used to display the content of the array displayed */
+	/* puts to display content of array displayed */
 	puts(displayed);
-	/* returning 0 will help us to exit the main function and know that the script is working just fine */
+	/* returning 0 will exit*/
 	return (0);
 }
