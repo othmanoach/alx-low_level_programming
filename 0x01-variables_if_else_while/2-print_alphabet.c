cd2cbccc37d653b7248121498f215de0f4338506
othmanoach <othmanoach@gmail.com>
2023-06-15 23:56:14 -0700

    This is a program that printing alphabet letter in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - This is a program that printing alphabet letter in lowercase
+*
+*Return: return 0 (means succes)
+*/
+int main(void)
+{
+	int alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		alphabet = tolower(alphabet);
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
