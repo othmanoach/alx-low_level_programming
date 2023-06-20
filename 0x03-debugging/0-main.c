968ce8af2fb269ba6285759cb04b2c2b7d2ba54a
othmanoach <othmanoach@gmail.com>
2023-06-20 12:50:02 -0700

    test if negative or positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - testing functions that prints integer if is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int integer;
+
+	integer = 0;
+	positive_or_negative(integer);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+traveling though debbeging world
