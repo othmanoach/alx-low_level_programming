8abdd0415490b44fe34d5cf6fce57506f01572a3
othmanoach <othmanoach@gmail.com>
2023-06-22 04:37:08 -0700

    printing diagonal lines

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of size s
- * @s: size of the square
+ * print_square - prints a square of size size
+ * @size: size of the square
  *
  * Return: void
  */
 
 
-void print_square(int s)
+void print_square(int size)
 {
-	int i, j;
+	int i, k;
 
-	if (s <= 0)
+	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < s; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (k = 0; k < (s); k++)
+		for (k = 0; k < (size); k++)
 		{
 			_putchar('#');
 		}
