5739e0a09462a30eb107b0621307f4c0ef249aa9
othmanoach <othmanoach@gmail.com>
2023-06-22 04:27:33 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - the main function
+ *
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+
+int i, n;
+	for (i = 1; i <= 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			_putchar('1');
+			_putchar(n % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
