9084a20f98575a522fcd5e32abf6cf2dab49938f
othmanoach <othmanoach@gmail.com>
2023-06-25 21:43:55 -0700

    swaping the value of a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of a and b
+* @a: first int
+* @b: second int
+* Return 0
+*/
+
+void swap_int(int *a, int *b)
+{
+		int s;
+
+		s = *a;
+		*a = *b;
+		*b = s;
+}
