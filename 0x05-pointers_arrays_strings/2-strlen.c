1039d3935252a3564e244b67872bc40c463e8dd8
othmanoach <othmanoach@gmail.com>
2023-06-25 21:45:55 -0700

    returning the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returning the length of a string
+ * @s: char check
+ * Description: will return the length of a string
+ * Return: 0 is always success
+ */
+ 
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
