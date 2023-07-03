b940ffa3b4f2a46c8d96c3edddfcc458e634f9a7
othmanoach <othmanoach@gmail.com>
2023-07-03 04:32:24 -0700

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
  * @s:first occurrence in the string
- * @accept: if no such byte matches @NULL, if else : one of the bytes
+ * @accept: if no such byte matches @NULL, if else one of the bytes
  */
 char *_strpbrk(char *s, char *accept)
 {
