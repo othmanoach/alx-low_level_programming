09a382b2a716e9bfb63390cb054a63fc8ec9f17a
othmanoach <othmanoach@gmail.com>
2023-07-05 04:41:31 -0700

    print the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _strlen_recursion - main function that returns tht length
+ * @s : a string
+ * Return : void (0)
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s+1);
+	}
+	return (i);
+}
+
