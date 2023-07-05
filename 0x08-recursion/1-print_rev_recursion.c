4ab8ccf15384c9b1bd6bb8e8fb6e3330200342fc
othmanoach <othmanoach@gmail.com>
2023-07-05 04:22:38 -0700

    printing strings in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - priting a string in reverse
+ * @s : a string
+ * Return : returning void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
