bc00f8c80bcd665e4903d3b8273c27e5cad940cb
othmanoach <othmanoach@gmail.com>
2023-08-07 05:56:32 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,21 +5,20 @@
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
- * @fn: integer
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fn;
+	int fd;
 	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	fn = open(filename, O_WRONLY | O_APPEND);
-	if (fn == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	len = write(fd, text_content, _strlen(text_content));
-	close(fn);
+	close(fd);
 	if (len == -1)
 		return (-1);
 	return (1);
