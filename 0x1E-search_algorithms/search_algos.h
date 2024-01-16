35659acc885d85151fb5a86366d4853da8511ac2
othmanoach <othmanoach@gmail.com>
2024-01-16 06:46:23 -0800

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,4 +30,12 @@ __local void print_array(int *array, int start, int stop)
 	printf("%d\n", array[i]);
 }
 
+typedef struct skiplist_s {
+    int n;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif
