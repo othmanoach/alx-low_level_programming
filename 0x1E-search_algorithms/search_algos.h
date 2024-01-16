d574cfb6abef1a5bc1ce5e7bae7469e93ce0419d
othmanoach <othmanoach@gmail.com>
2024-01-16 10:25:35 -0800

    search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,15 +30,24 @@ __local void print_array(int *array, int start, int stop)
 	printf("%d\n", array[i]);
 }
 
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 /**
- * singly linked list node structure
-*/ 
-typedef struct skiplist_s {
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
     int n;
+    size_t index;
     struct skiplist_s *next;
     struct skiplist_s *express;
 } skiplist_t;
-
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
